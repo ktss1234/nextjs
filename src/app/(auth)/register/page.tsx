ff904d47c85f1267aa8e262e82bf748ac40ecb8b
ktss1234 <sak1234@hotmail.com>
2023-12-19 12:03:12 +0700

    ep34 register validation

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,8 @@ import { Box, Card, CardContent, InputAdornment, TextField, Typography, Button }
 import React from 'react'
 import * as Icons from "@mui/icons-material/";
 import { Controller, useForm } from "react-hook-form";
-
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 interface User {
   username: string;
   password: string;
@@ -14,35 +15,45 @@ type Props = {}
 
 export default function Register({ }: Props) {
   const initialValue: User = { username: "", password: "" };
-  const { control, handleSubmit } = useForm<User>({ defaultValues: initialValue })
+  const formValidateSchema = Yup.object().shape({
+    username: Yup.string().required("Username is required").trim(),
+    password: Yup.string().required("Password is required").trim(),
+  });
+  const { control, handleSubmit, formState: { errors } } = useForm<User>({ defaultValues: initialValue, resolver: yupResolver(formValidateSchema) })
+
   const showForm = () => {
     return <form onSubmit={handleSubmit((value: User) => {
       alert(JSON.stringify(value))
     })}>
       {/* Username */}
 
-      <Controller name="username" control={control} render={({ field }) => (<TextField
-        {...field}
-        variant="outlined"
-        margin="normal"
-        fullWidth
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <Icons.Email />
-            </InputAdornment>
-          ),
-        }}
-        label="Username"
-        autoComplete="email"
-        autoFocus
-      />)
+      <Controller name="username" control={control} render={({ field }) => (
+        <TextField
+          {...field}
+          error={(errors.username?.message ?? "") != ''}
+          helperText={errors.username?.message?.toString()}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Icons.Email />
+              </InputAdornment>
+            ),
+          }}
+          label="Username"
+          autoComplete="email"
+          autoFocus
+        />)
 
       }></Controller>
       {/* password */}
       <Controller name="password" control={control} render={({ field }) => (
         <TextField
           {...field}
+          error={(errors.password?.message ?? "") != ''}
+          helperText={errors.password?.message?.toString()}
           variant="outlined"
           margin="normal"
           fullWidth

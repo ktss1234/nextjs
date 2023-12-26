f6823277d1c519a734a65b6f4d06e670e666f9e7
ktss1234 <sak1234@hotmail.com>
2023-12-26 14:53:54 +0700

    ep59 auth flow

--- src/app/_conponents/common/AuthProviders.tsx
+++ src/app/_conponents/common/AuthProviders.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+
+import { getSession, userSelector } from "@/store/slices/userSlice";
+import { store } from "@/store/store";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 type Props = {};
 
@@ -10,19 +14,35 @@ export default function AuthProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
+  useEffect(() => {
+    store.dispatch(getSession());
+  }, []);
+
+
   const path = usePathname();
-  useEffect(()=>{
-    initialize();
-  })
-  const initialize = ()=>{
-  if (path == "/") {
-    router.push("/stock");
+  const router = useRouter();
+  const userReducer = useSelector(userSelector); 
+
+  // is fetching session (eg. show spinner)
+  if (userReducer.isAuthenticating) {
     return null;
   }
+  // If user is not logged in, return login component
+  if (path !== "/login" && path !== "/register") {
+    if (!userReducer.isAuthenticated) {
+      router.push(`/login`);
+      return null;
+    } else if (path == "/") {
+      router.push(`/stock`); // default page after login when call root path
+      return null;
+    }
+  } else {
+    if (userReducer.isAuthenticated) {
+      router.push(`/stock`); // default page after login
 
+      return null;
+    }
   }
-
 
 
   return <div>{children}</div>;

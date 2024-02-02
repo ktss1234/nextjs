985003206bc0045ebecf762d7e04e420cb2863ad
ktss1234 <sak1234@hotmail.com>
2024-02-02 14:15:47 +0700

    ep68 action column

--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -10,82 +10,111 @@ import { productImageURL } from "@/utils/commonUtil";
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
 import { NumericFormat } from "react-number-format";
-import { Fab, Link, Typography } from "@mui/material";
+import { Fab, IconButton, Link, Stack, Typography } from "@mui/material";
 import dayjs from "dayjs";
-import { Add } from "@mui/icons-material";
-
-
-const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    {
-        field: 'image', headerName: 'Image', width: 130,
-        renderCell: ({ value }:
-            GridRenderCellParams) =>
-        (
-            <Zoom>
-                <Image
-                    key={value}
-                    height={500}
-                    width={500}
-                    alt="product image"
-                    src={productImageURL(value)}
-                    style={{
-                        width: 70,
-                        height: 70,
-                        borderRadius: "5%",
-                        objectFit: "cover",
-                    }}
-                />
-            </Zoom>
-        )
-    },
-    {
-        field: 'name', headerName: 'Name', width: 350
-    },
-    {
-        field: 'stock', headerName: 'Stock', width: 150,
-        renderCell: ({ value }: GridRenderCellParams) => (
-            <Typography variant="body1">
-                <NumericFormat
-                    value={value}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    decimalScale={0}
-                    fixedDecimalScale={true} />
-            </Typography>
-
-        )
-    },
-    {
-        field: 'price', headerName: 'Price', width: 130,
-        renderCell: ({ value }: GridRenderCellParams) => (
-            <Typography variant="body1">
-                <NumericFormat
-                    value={value}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    decimalScale={2}
-                    fixedDecimalScale={true} />
-            </Typography>
-
-        )
-    },
-    {
-        field: "createdAt",
-        headerName: "Timestamp",
-        width: 230,
-        renderCell: ({ value }) => (
-            <Typography variant="body1">
-                {dayjs(value).format("DD/MM/YYYY HH:mm")}
-            </Typography>
-        ),
-    },
-];
-
+import { Add, Delete, Edit } from "@mui/icons-material";
+import router from "next/router";
 
 export default function StockPage() {
     const productReducer = useSelector(productSelector)
     const dispatch = useAppDispatch()
+
+    
+    const columns: GridColDef[] = [
+        { field: 'id', headerName: 'ID', width: 90 },
+        {
+            field: 'image', headerName: 'Image', width: 130,
+            renderCell: ({ value }:
+                GridRenderCellParams) =>
+            (
+                <Zoom>
+                    <Image
+                        key={value}
+                        height={500}
+                        width={500}
+                        alt="product image"
+                        src={productImageURL(value)}
+                        style={{
+                            width: 70,
+                            height: 70,
+                            borderRadius: "5%",
+                            objectFit: "cover",
+                        }}
+                    />
+                </Zoom>
+            )
+        },
+        {
+            field: 'name', headerName: 'Name', width: 350
+        },
+        {
+            field: 'stock', headerName: 'Stock', width: 150,
+            renderCell: ({ value }: GridRenderCellParams) => (
+                <Typography variant="body1">
+                    <NumericFormat
+                        value={value}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        decimalScale={0}
+                        fixedDecimalScale={true} />
+                </Typography>
+
+            )
+        },
+        {
+            field: 'price', headerName: 'Price', width: 130,
+            renderCell: ({ value }: GridRenderCellParams) => (
+                <Typography variant="body1">
+                    <NumericFormat
+                        value={value}
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        decimalScale={2}
+                        fixedDecimalScale={true} />
+                </Typography>
+
+            )
+        },
+        {
+            field: "createdAt",
+            headerName: "Timestamp",
+            width: 230,
+            renderCell: ({ value }) => (
+                <Typography variant="body1">
+                    {dayjs(value).format("DD/MM/YYYY HH:mm")}
+                </Typography>
+            ),
+        },
+        {
+            headerName: "ACTION",
+            field: ".",
+            width: 120,
+            renderCell: ({ row }: GridRenderCellParams<any>) => (
+                <Stack direction="row">
+                    <IconButton
+                        aria-label="edit"
+                        size="large"
+                        onClick={() => router.push(`/stock/edit?id=${row.id}`)}
+                    >
+                        <Edit fontSize="inherit" />
+                    </IconButton>
+                    <IconButton
+                        aria-label="delete"
+                        size="large"
+                        onClick={() => {
+                            // setSelectedProduct(row);
+                            // setOpenDialog(true);
+                        }}
+                    >
+                        <Delete fontSize="inherit" />
+                    </IconButton>
+                </Stack>
+            ),
+        },
+    ];
+
+
+
     React.useEffect(() => {
         dispatch(getProducts())
     }, [dispatch])

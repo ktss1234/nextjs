fcb0166bfb1d22f04395e6d8dc7ed0d15ae9879c
ktss1234 <sak1234@hotmail.com>
2024-02-06 12:20:23 +0700

    ep74 middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,31 @@
+// middleware.ts
+
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+import { ACCESS_TOKEN_KEY } from "./utils/constant";
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/stock", request.url));
+export default function middleware(request: NextRequest) {
+  const cookieStore = cookies();
+  const accessTokenKey = cookieStore.get(ACCESS_TOKEN_KEY);
+  const path = request.nextUrl.pathname;
+
+  if (accessTokenKey && accessTokenKey.value) {
+    if (path == "/login" || path == "/register" || path == "/") {
+      return NextResponse.redirect(new URL("/stock", request.url));
+    }
+
+    return NextResponse.next({
+      request,
+    });
+  } else if (path != "/login" && path != "/register") {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next({
+    request,
+  });
 }
-
-// See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/login/:path*",
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };

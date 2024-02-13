9145d6a99040df7da516dbab46d072b105b29d0c
ktss1234 <sak1234@hotmail.com>
2024-02-13 09:44:58 +0700

    ep82 setup chartjs

--- src/app/(routes)/report/page.tsx
+++ src/app/(routes)/report/page.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-type Props = {}
+function getRamdomBetween0And1000() {
+  const randomNumber = Math.random();
 
-export default function Report({}: Props) {
-  return (
-    <div>Report</div>
-  )
+  // คูณด้วย 1000 
+  const randomNumberBetween0And1000 = randomNumber * 1000;
+
+  // ปัดเศษลงเป็นจำนวนเต็ม
+  const randomNumberInt = Math.floor(randomNumberBetween0And1000);
+  return randomNumberInt
+}
+
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: labels.map(() => getRamdomBetween0And1000()),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: labels.map(() => getRamdomBetween0And1000()),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+export default function Report() {
+  return <Bar options={options} data={data} />;
 }

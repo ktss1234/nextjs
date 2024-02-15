fc9065a182d8684f7fee6325afac980e00c1313b
ktss1234 <sak1234@hotmail.com>
2024-02-15 12:52:04 +0700

    94

--- src/app/(routes)/stock/edit/page.tsx
+++ src/app/(routes)/stock/edit/page.tsx
@@ -2,6 +2,8 @@ import * as Yup from "yup";
 import StockEditForm from "./StockEditForm";
 import { ProductData } from "@/models/product.model";
 import { doGetStockById } from "@/services/serviceServices";
+import { Metadata, ResolvingMetadata } from "next";
+import { productImageURL } from "@/utils/commonUtil";
 
 const formValidateSchema = Yup.object().shape({
   name: Yup.string().required("Name is required").trim(),
@@ -14,7 +16,41 @@ type Props = {
     id?: string;
   };
 };
+export async function generateMetadata(
+  { searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let product = {} as ProductData;
 
+  if (searchParams.id) {
+    product = await doGetStockById(searchParams.id);
+  }
+
+  return {
+    title: product.name ?? "StockEdit",
+    openGraph: {
+      title: product.name,
+      description: product.name + "price: " + product.price,
+      url: "https://nextjs.org",
+      siteName: "Next.js",
+      images: [
+        {
+          url: productImageURL(product.image),
+          width: 800,
+          height: 600,
+        },
+        {
+          url: "https://nextjs.org/og-alt.png",
+          width: 1800,
+          height: 1600,
+          alt: "My custom alt",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+  };
+}
 export default async function StockEdit({ searchParams }: Props) {
   let product = {} as ProductData;
   if (searchParams.id) {
